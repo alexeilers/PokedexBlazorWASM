7b69315220b57bd03bf6a35ff9ba31e428d927d2
Eilers Fam <alexander.eilers@gmail.com>
2022-04-19 11:19:03 -0400

    updated Index.razor

--- Pokedex/Models/Pokemon.cs
+++ Pokedex/Models/Pokemon.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace Pokedex.Models
 {
     public class Pokemon
     {
-        public Pokemon()
-        {
-        }
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("weight")]
+        public int Weight { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
+        [JsonProperty("base_experience")]
+        public int Experience { get; set; }
     }
 }
